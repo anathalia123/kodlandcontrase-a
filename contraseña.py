80e484d75839584ee57f31b5464dd78bd479db95
anathalia123 <anathaliaariasrosero@gmail.com>
2024-11-16 09:37:44 -0500

    Create contraseña.py

---
+++ contraseña.py
@@ -0,0 +1,8 @@
+import random
+caracteres= "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+longitud= int(input("Ingresa la longitud de tu contraseña"))
+resultado=""
+for i in range(longitud):
+    resultado+=random.choice(caracteres)
+
+print(f"tu contraseña con una longitud de {longitud} es: {resultado}")
